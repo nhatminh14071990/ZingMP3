7044548ee03cd423a7648550ac626dadbcbbda05
vanhs <nhatminh14071990@gmail.com>
2017-09-19 14:43:18 +0700

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+ZingMP3 version 1
+Author vanhs
+Write on Xcode 8.3.3, Swift 3.1, test on iphone 6
--- ZingMP3/ViewController.swift
+++ ZingMP3/ViewController.swift
@@ -13,13 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        logo.alpha = 0
+        logoText.alpha = 0
+        fullname.alpha = 0
+        
+//        print("viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+//        print("viewWillAppear")
+        
+        super.viewWillAppear(animated)
+        
+        UIView.animate(withDuration: 1, animations: {
+            self.logo.alpha = 1
+        }) { (finished) in
+            UIView.animate(withDuration: 1, animations: {
+                self.logoText.center = CGPoint(x: self.logo.center.x, y: 50)
+                self.logoText.alpha = 1
+            }) { (finished) in
+                UIView.animate(withDuration: 1, animations: {
+                    self.fullname.alpha = 1
+                    self.fullname.center = CGPoint(x: self.logo.center.x, y: 600)
+                })
+            }
+        
+        }
+    }
 
 
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var logoText: UILabel!
+    
+    @IBOutlet weak var fullname: UILabel!
+    
+    
+    
 }
 
